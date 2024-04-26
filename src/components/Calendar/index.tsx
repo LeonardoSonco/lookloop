d1afdc3b3ef2113b140cae619b36b7811be6b6db
LeonardoSonco <leonardokarling@hotmail.com>
2024-04-26 13:24:49 -0300

    fit(): Modificando calendário.

--- src/components/Calendar/index.tsx
+++ src/components/Calendar/index.tsx
@@ -1,34 +1,67 @@
-
-import { Calendar, momentLocalizer } from 'react-big-calendar';
-import moment from 'moment';
-import 'react-big-calendar/lib/css/react-big-calendar.css';
-import './index.css';
+import { Calendar, momentLocalizer } from "react-big-calendar";
+import moment from "moment";
+import "react-big-calendar/lib/css/react-big-calendar.css";
+import "./index.css";
 
 const localizer = momentLocalizer(moment);
+
+console.log(moment().add(90, "minutes").toDate());
 
 const MyCalendar = () => {
   const events = [
     {
-      title: 'Valmir - Corte Degrade + Barba',
-      start: moment().toDate(),
-      end: moment().add(90, 'minutes').toDate(),
+      title: "Valmir Jr - Corte Degrade + Barba",
+      start: moment("Apr 25 2024 13:30:00").toDate(),
+      end: moment("Apr 25 2024 13:30:00").add(60, "minutes").toDate(),
+    },
+    {
+      title: "Carlos - Corte Degrade",
+      start: moment("Apr 25 2024 15:00:00").toDate(),
+      end: moment("Apr 25 2024 15:00:00").add(30, "minutes").toDate(),
+    },
+    {
+      title: "Edu - Corte Degrade + Barba",
+      start: moment("Apr 25 2024 16:00:00").toDate(),
+      end: moment("Apr 25 2024 16:00:00").add(30, "minutes").toDate(),
+    },
+    {
+      title: "Lala - Corte Degrade + Barba",
+      start: moment("Apr 25 2024 17:00:00").toDate(),
+      end: moment("Apr 25 2024 17:00:00").add(30, "minutes").toDate(),
+    },
+
+    {
+      title: "Leonardo - Corte Degrade + Barba",
+      start: moment("Apr 24 2024 09:30:00").toDate(),
+      end: moment("Apr 24 2024 09:30:00").add(45, "minutes").toDate(),
+    },
+    {
+      title: "Rodrigo - Corte Degrade + Barba",
+      start: moment("Apr 26 2024 11:00:00").toDate(),
+      end: moment("Apr 26 2024 11:00:00").add(30, "minutes").toDate(),
     },
   ];
 
   return (
-    <div style={{ height: 600, width: 1100, overflow: 'hidden' }}>
-      <div style={{ height: '100%', overflowY: 'scroll', scrollbarWidth: 'none' }}>
-        <Calendar
-          localizer={localizer}
-          events={events}
-          defaultView="week"
-          selectable
-          popup
-          style={{ height: 600, width: 1100 }}
-        />
-      </div>
+    <div style={{ height: 600, width: "90vw", overflow: 'hidden', maxWidth: 1100 }}>
+    <Calendar
+      localizer={localizer}
+      step={15}
+      min={new Date(2024, 10, 0, 8, 0, 0)}
+      max={new Date(2024, 10, 0, 22, 0, 0)}
+      events={events}
+      defaultView="week"
+    />
     </div>
   );
 };
 
 export default MyCalendar;
+
+/* 
+
+<div style={{ height: 600, width: 1100, overflow: 'hidden' }}>
+      <div style={{ height: '100%', overflowY: 'scroll', scrollbarWidth: 'none' }}>
+       
+      </div>
+    </div>*/
--- src/pages/CompanyAgendaPage/index.tsx
+++ src/pages/CompanyAgendaPage/index.tsx
@@ -1,4 +1,3 @@
-
 import MyCalendar from "../../components/Calendar";
 import { useMenuContext } from "../../context/MenuContext";
 
@@ -10,28 +9,12 @@ const CompanyAgendaPage: React.FC = () => {
         isNavOpen ? "pl-52 ml-1.5" : ""
       }`}
     >
-      <div className="bg-purple_light py-2 text-white flex  gap-5 px-5 rounded-3xl">
-        <div>
-          {" "}
-          <h5 className="border-r-2  pr-4 text-base max-w-72">
-            <span className="font-bold">Próximo serviço:</span> Corte Degrade +
-            Barba
-          </h5>
-        </div>
-
-        <div className="font-bold flex flex-col justify-center">
-          <p className="text-xl">Dia 21</p>
-          <span className="text-base">08:00 AM</span>
-        </div>
-      </div>
-
-      <div>
-        <p>Serviço atual: Corte Degrade em Valmir Thume</p>
+      <div className="mt-5 flex justify-center">
         <div
           id="calendar"
-          className="my-10 mx-5 shadow-shadowBox rounded-3xl p-8"
+          className="my-10 mx-10 shadow-shadowBox rounded-3xl p-8 "
         >
-         <MyCalendar />
+          <MyCalendar />
         </div>
       </div>
     </section>
@@ -39,3 +22,39 @@ const CompanyAgendaPage: React.FC = () => {
 };
 
 export default CompanyAgendaPage;
+
+/*
+
+<div className="w-fit bg-purple_light py-2 text-white flex gap-5 px-5 rounded-3xl">
+            <div>
+              {" "}
+              <h5 className="border-r-2  pr-4 text-base max-w-72">
+                <span className="font-bold">Próximo serviço:</span> Corte
+                Degrade + Barba
+              </h5>
+            </div>
+
+            <div className="font-bold flex flex-col justify-center">
+              <p className="text-xl">Dia 21</p>
+              <span className="text-base">08:00 AM</span>
+            </div>
+          </div>
+
+            <div className="w-full flex justify-end">
+          <div className="w-fit bg-purple_light py-2 text-white flex gap-5 px-5 rounded-3xl">
+            <div>
+              {" "}
+              <h5 className="border-r-2  pr-4 text-base max-w-72">
+                <span className="font-bold">Próximo serviço:</span> Corte Degrade
+                em Carlos
+              </h5>
+            </div>
+
+            <div className="font-bold flex flex-col justify-center">
+              <p className="text-xl">Dia 25</p>
+              <span className="text-base">13:30 PM</span>
+            </div>
+          </div>
+        </div>
+
+*/
