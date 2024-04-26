434c05dbfa55c29ed7d88ca69a1a8eb945856e12
LeonardoSonco <leonardokarling@hotmail.com>
2024-04-26 14:55:36 -0300

    feat(): Adicionando Page do funcionario.

--- src/pages/CompanyEmployeePage/index.tsx
+++ src/pages/CompanyEmployeePage/index.tsx
@@ -1,14 +1,33 @@
+import { PlusSquare } from "react-feather";
 import CardEmployee from "../../components/CardEmployee";
 import { useMenuContext } from "../../context/MenuContext";
 
-const CompanyEmployeePage : React.FC = () => {
-    const { isNavOpen } = useMenuContext();
-    return <section id="companyEmployee" className={`flex flex-col justify-center items-center  ${
+const CompanyEmployeePage: React.FC = () => {
+  const { isNavOpen } = useMenuContext();
+  return (
+    <section
+      id="companyEmployee"
+      className={`flex mt-10 justify-center  ${
         isNavOpen ? "pl-52 ml-1.5" : ""
-      }`}>
+      }`}
+    >
+      <div className="flex flex-col gap-6">
         <CardEmployee />
-        
+        <CardEmployee />
+        <div className="flex gap-2 justify-end">
+          <span className="text-purple text-lg font-semibold ">
+            Adicionar Funcionário
+          </span>
+          <PlusSquare
+            color="#ffffff"
+            fill="#413B6B"
+            size={30}
+            strokeWidth={1}
+          />
+        </div>
+      </div>
     </section>
-}
+  );
+};
 
 export default CompanyEmployeePage;
